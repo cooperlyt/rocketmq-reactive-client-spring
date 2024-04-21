c7eeb65ed5d025c6c0c4151dd6aea56b72407788
cooper <lyt3131@163.com>
2024-04-22 07:10:11 +0800

    Init

--- rocketmq-reactive-client-spring-boot/src/main/kotlin/io/github/cooperlyt/rocketmq/client/ConsumerResult.kt
+++ rocketmq-reactive-client-spring-boot/src/main/kotlin/io/github/cooperlyt/rocketmq/client/ConsumerResult.kt
@@ -1,8 +1,5 @@
 package io.github.cooperlyt.rocketmq.client
 
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus
-import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus
-
 enum class ConsumerResult {
     SUCCESS,
     FAILURE
